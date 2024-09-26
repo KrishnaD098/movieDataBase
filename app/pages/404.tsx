881389a9e1cbad2c8d845786e4b6575380b55659
Krishna <dhootkrishna123@gmail.com>
2024-09-26 18:15:55 +0530

    updates on seo

--- app/pages/404.tsx
+++ app/pages/404.tsx
@@ -1,13 +1,19 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 const Custom404 = () => (
-  <div className="min-h-screen flex flex-col justify-center items-center text-center">
-    <h1 className="text-4xl font-bold">Page Not Found</h1>
-    <p className="text-lg my-4">Sorry, the page you&apos;re looking for doesn&apos;t exist.</p>
-    <Link href="/" className="text-blue-600">
-      Go back to Homepage
-    </Link>
-  </div>
+  <>
+    <Head>
+      <meta name="robots" content="noindex" />
+    </Head>
+    <div className="min-h-screen flex flex-col justify-center items-center text-center">
+      <h1 className="text-4xl font-bold">Page Not Found</h1>
+      <p className="text-lg my-4">Sorry, the page you&apos;re looking for doesn&apos;t exist.</p>
+      <Link href="/" className="text-blue-600">
+        Go back to Homepage
+      </Link>
+    </div>
+  </>
 );
 
 export default Custom404;
